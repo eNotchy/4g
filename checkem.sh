3cbe1762b0f49e4729ee6702148647db249623fb
eNotchy <118693467+eNotchy@users.noreply.github.com>
2026-01-12 22:09:04 +0100

    Update README.org and add native-compile test

--- checkem.sh
+++ checkem.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 emacs -Q --batch -f batch-byte-compile 4g.el
+# REVIEW: Don't attempt native-compile on platforms where it doesn't make sense. Which are...?
+emacs -Q --batch -f batch-native-compile 4g.el
 emacs -Q --batch -l checkdoc --eval '(let ((checkdoc-force-docstrings-flag nil)) (checkdoc-file "4g.el"))'
 emacs -Q --batch -L . -l 4g.el -l test/4g-tests.el -f ert-run-tests-batch-and-exit
