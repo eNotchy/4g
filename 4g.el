0ac6f32ff547d0c3d5576c46ad13e6f5fc9b1ca4
eNotchy <118693467+eNotchy@users.noreply.github.com>
2026-01-15 14:54:52 +0100

    Remove threadno from op line in catalog
    Not needed anymore, I did the thread completions in a different way.

--- 4g.el
+++ 4g.el
@@ -1107,8 +1107,8 @@ From catalogs, this is called with a BOARD arg. From threads, without."
            (space (if (numberp align-to)
                       (propertize " " 'display `(space :align-to ,align-to))
                     " "))
-           (stats (format "[ %s R %3s I %3s ]%s%s%s"
-                          no r-str i-str pin lock old)))
+           (stats (format "[ %3s R %3s I ]%s%s%s"
+                          r-str i-str pin lock old)))
       (if flip
           (concat l-str space stats)
         (concat stats space l-str)))))
